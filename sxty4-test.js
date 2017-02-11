46ed45d49498f503fe4ad1465bd802c783507882
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-11 13:06:09 +0800

    Updated test.

--- sxty4-test.js
+++ sxty4-test.js
@@ -4,6 +4,6 @@ const sxty4 = require( "./sxty4.js" );
 
 let password = sxty4( "hello" ).encode( );
 let showpassword = sxty4( password ).decode( );
-
 console.log( password );
 console.log( showpassword );
+console.log( sxty4( ) );
